76fa300ad4a7ec3eb171fdbd6a505da31aaf9f64
Sokol Decani <Sokol.Decani@emea-bottomline.root.bottomline.com>
2017-03-06 11:37:12 +0100

    Initial code base

---
+++ src/main/java/com/ekipa/FileIoApplication.java
@@ -0,0 +1,12 @@
+package com.ekipa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileIoApplication.class, args);
+	}
+}
